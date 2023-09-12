f6b09300cae5a92d3f71425452dab6b6c666a4e6
Evan Edwards <40186280+SHIGGY7788@users.noreply.github.com>
2023-09-12 13:36:26 -0700

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("myBtn")
+var btn2 = document.getElementById("myBtn2")
+var btn3 = document.getElementById("myBtn3")
+var span = document.getElementsByClassName("close")[0]
+
+
+btn.onclick = function() {
+    modal.style.display = 'block';
+}
+btn2.onclick = function() {
+    modal.style.display = 'block';
+}
+btn3.onclick = function() {
+    modal.style.display = 'block';
+}
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
